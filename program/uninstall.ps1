2e4cf5e5461326a4c4f6fbee9f1d8f1516fa131a
Lynquity <Lynquity@outlook.de>
2024-11-03 00:48:39 +0100

    Update uninstall.ps1

--- program/uninstall.ps1
+++ program/uninstall.ps1
@@ -1,1 +1,9 @@
-
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    # If not admin, restart PowerShell as admin
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"";
+    $newProcess.Verb = "runas"; # Force PowerShell to run as administrator
+    [System.Diagnostics.Process]::Start($newProcess) | Out-Null;
+    Exit;
+}
