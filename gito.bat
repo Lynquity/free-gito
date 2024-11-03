cad5167c7268263c31798a5f89f929e443799a28
Lynquity <Lynquity@outlook.de>
2024-11-03 01:04:36 +0100

    Update gito.bat

--- gito.bat
+++ gito.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if %1 == "--uninstall" (
-    powershell -ExecutionPolicy Bypass -File "C:/Windows/System32/ELW/gito/program/uninstall.ps1"
+    powershell -ExecutionPolicy Bypass -File "C:/Windows/System32/ELW/gito/uninstall.ps1"
 )
 else (
     python C:/Windows/System32/ELW/gito/main.py
