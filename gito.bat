c30cd231ec610a4877b214129970b71398e65879
Jamie Poeffel <Jamie_Poeffel@sluz.ch>
2024-11-09 00:33:38 +0100

    Paths changed

--- gito.bat
+++ gito.bat
@@ -3,5 +3,7 @@
 if "%1"=="--delete" (
 	powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\uninstall.ps1"
 )
-python C:\Windows\System32\program\main.py
+else (
+	python C:\Windows\System32\ELW\gito\main.py
+)
 
