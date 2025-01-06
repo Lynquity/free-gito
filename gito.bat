5b0b7ede698f2d0e92eb3e69a12d890ae4c1ae8f
Lynquity <Lynquity@outlook.de>
2025-01-06 21:58:41 +0100

    Update gito.bat

--- gito.bat
+++ gito.bat
@@ -3,7 +3,8 @@
 if "%1"=="--delete" (
     powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\uninstall.ps1"
 ) else if "%1"=="sync" (
-    powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\sync.ps1
+    powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\sync.ps1"
+
 ) else (
     C:\Windows\System32\ELW\gito\main.exe
 )
