4f353d60aad34d3e3ad8028b1772aa6ffc9d9942
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-02-05 21:02:02 +0100

    Add support for 'm' command in gito.bat to execute more.exe

--- gito.bat
+++ gito.bat
@@ -4,6 +4,8 @@ if "%1"=="--delete" (
     powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\uninstall.ps1"
 ) else if "%1"=="sync" (
     powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\sync.ps1"
+) else if "%1"=="m" (
+    C:\Windows\System32\ELW\gito\more.exe
 ) else (
     C:\Windows\System32\ELW\gito\main.exe
 )
