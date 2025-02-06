361f35044605c7f99306a82e504fdf053084025b
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-02-06 12:29:55 +0100

    Geändert --> gito.bat

--- gito.bat
+++ gito.bat
@@ -9,6 +9,9 @@ if "%1"=="--delete" (
 ) else if "%1"=="m" (
     powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\checkversion.ps1"
     C:\Windows\System32\ELW\gito\more.exe
+) else if "%1"=="a" (
+    powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\checkversion.ps1"
+    C:\Windows\System32\ELW\gito\ai.exe
 ) else (
     powershell -ExecutionPolicy Bypass -File "C:\Windows\System32\ELW\gito\checkversion.ps1"
     C:\Windows\System32\ELW\gito\main.exe
